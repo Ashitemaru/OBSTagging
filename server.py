c943617416406f926baafc7079486d81e0c0b20f
Ashitemaru <qhd19@mails.tsinghua.edu.cn>
2020-10-10 11:32:36 +0800

    Function: bugs fixed

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from application import application
 
 from tornado.options import define, options
 
-define("port", default=8080, type=int, help="Port to listen.")
+define("port", default=1926, type=int, help="Port to listen.")
 define("static", default="./static/", help="entry point of static files")
 define("debug", default=True, help="run in debug mode")
 define("num_inst_per_group", default=20, type=int, help="Number of instances per group.")
